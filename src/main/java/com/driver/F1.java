41a9bb335f994558635f32a7105c5892da7970aa
skstlenovo <116804212+skstlenovo@users.noreply.github.com>
2023-05-06 09:30:33 +0530

    3rd commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,4,4,6,isManual,"gg",7);
+        super(name,4,5,6,isManual,"gg",7);
 
     }
 
